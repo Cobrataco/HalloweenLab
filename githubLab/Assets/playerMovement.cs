0567ce8c89ba53c2c41ea30b869618fab105ff22
Cobrataco <arricketts@mail.csuchico.edu>
2023-10-31 11:36:27 -0700

    update player movement

--- githubLab/Assets/playerMovement.cs
+++ githubLab/Assets/playerMovement.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        //moves player
     }
 }
